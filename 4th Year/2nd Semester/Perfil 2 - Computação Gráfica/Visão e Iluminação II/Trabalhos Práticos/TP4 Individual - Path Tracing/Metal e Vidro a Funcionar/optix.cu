86939e15c10faff9b17cdf8dbc30a9c353577b1c
Diogo Nogueira <34174814+diogoesnog@users.noreply.github.com>
2020-07-25 15:24:14 +0100

    Update Projeto VI II

--- 4th Year/2nd Semester/Perfil 2 - Computação Gráfica/Visão e Iluminação II/Trabalhos Práticos/TP4 Individual - Path Tracing/Metal e Vidro a Funcionar/optix.cu
+++ 4th Year/2nd Semester/Perfil 2 - Computação Gráfica/Visão e Iluminação II/Trabalhos Práticos/TP4 Individual - Path Tracing/Metal e Vidro a Funcionar/optix.cu
@@ -32,6 +32,9 @@ struct RadiancePRD
     int32_t     countEmitted;
 };
 
+// ############################################################
+// Radiance Rays
+
 // Esta função não sofre alterações
 extern "C" __global__ void __closesthit__radiance() 
 {
@@ -134,154 +137,19 @@ extern "C" __global__ void __anyhit__radiance()
 
 // Miss usada para definir a Cor de Fundo
 // Cor mudada de float3(0,0,0) para float3(1,1,1)
-// Fundo branco. Nota-se no Reflexo do Objecto.
+// Fundo branco. Nota-se no Reflexo do Objecto
 extern "C" __global__ void __miss__radiance()
 {
     RadiancePRD &prd = *(RadiancePRD*)getPRD<RadiancePRD>();
+    
     // Cor de Fundo representada pelo Float3
     prd.radiance = make_float3(1.0f, 1.0f, 1.0f);
     prd.done = true;
 }
 
-// 1. Shadow Rays
+//Funções Usadas para o Vidro
 
-extern "C" __global__ void __closesthit__shadow()
-{
-    optixSetPayload_0( static_cast<uint32_t>(true));
-}
-
-// any hit for shadows
-extern "C" __global__ void __anyhit__shadow()
-{
-}
-
-// miss for shadows
-extern "C" __global__ void __miss__shadow()
-{
-    optixSetPayload_0( static_cast<uint32_t>(false));
-}
-
-//METAL
-
-extern "C" __global__ void __closesthit__radiance_metal()
-{
-
-    const TriangleMeshSBTData &sbtData
-      = *(const TriangleMeshSBTData*)optixGetSbtDataPointer();  
-
-
-    RadiancePRD &prd = *(RadiancePRD *)getPRD<RadiancePRD>();
-
-    // retrieve primitive id and indexes
-    const int   primID = optixGetPrimitiveIndex();
-    const uint3 index  = sbtData.index[primID];
-
-    // get barycentric coordinates
-    const float u = optixGetTriangleBarycentrics().x;
-    const float v = optixGetTriangleBarycentrics().y;
-
-    // compute normal
-    const float4 n
-        = (1.f-u-v) * sbtData.vertexD.normal[index.x]
-        +         u * sbtData.vertexD.normal[index.y]
-        +         v * sbtData.vertexD.normal[index.z];
-
-    const float3 normal = normalize(make_float3(n));
-    float3 normRayDir = optixGetWorldRayDirection();
-
-    // intersection position
-    const float3 &rayDir =  optixGetWorldRayDirection();
-    const float3 pos = optixGetWorldRayOrigin() + optixGetRayTmax() * rayDir;
-
-    if (prd.countEmitted && length(sbtData.emission) != 0) {
-        prd.emitted = sbtData.emission;
-        return;
-    }
-    else
-        prd.emitted = make_float3(0.0f);
-
-    //declaração do seed  
-    uint32_t seed = prd.seed;
-    const float z1 = rnd(seed);
-    const float z2 = rnd(seed);
-    prd.seed = seed;
-
-    RadiancePRD afterPRD;
-    afterPRD.radiance = make_float3(1.0f);
-    afterPRD.seed = prd.seed;
-    uint32_t u0, u1;
-    packPointer( &afterPRD, u0, u1 );
-    
-    //Achar o número de raios brilhantes, e o grau de brilho
-    const float glossiness = optixLaunchParams.global->glossiness;
-    const int glossy_rays = optixLaunchParams.global->glossyRays;
-    float3 glossy = make_float3(0.0f);
-    float3 direcaoL;
-    float3 reflectDir = reflect(optixGetWorldRayDirection(), normal);
-    
-    //Lançar os raios
-    for (int i = 0; i < glossy_rays; ++i) {
-        afterPRD.radiance = make_float3(1.0f);
-        do{
-            const float z1 = rnd(seed);
-            const float z2 = rnd(seed);
-            prd.seed = seed;
-        
-            cosine_power_sample_hemisphere( z1, z2, direcaoL, glossiness );
-            Onb onb( reflectDir );
-            onb.inverse_transform( direcaoL );
-            prd.direction = direcaoL;
-            prd.origin    = pos;
-            prd.attenuation *= sbtData.diffuse ;
-           // prd.countEmitted = false;
-        } while (dot(direcaoL, normal) < 0.001);
-    
-        glossy += afterPRD.radiance;
-    
-    }
-
-    const float3 lightV1 = make_float3(0.47f, 0.0, 0.0f);
-    const float3 lightV2 = make_float3(0.0f, 0.0, 0.38f);
-    const float3 light_pos = make_float3(optixLaunchParams.global->lightPos) + lightV1 * z1 + lightV2 * z2;
-
-    // Calculate properties of light sample (for area based pdf)
-    const float  Ldist = length(light_pos - pos );
-    const float3 L     = normalize(light_pos - pos );
-    const float  nDl   = dot( normal, L );
-    const float3 Ln    = normalize(cross(lightV1, lightV2));
-    const float  LnDl  = -dot( Ln, L );
-
-    float weight = 0.0f;
-    if( nDl > 0.0f && LnDl > 0.0f )
-    {
-    uint32_t occluded = 0u;
-        optixTrace(optixLaunchParams.traversable,
-            pos,
-            L,
-            0.001f,         // tmin
-            Ldist - 0.01f,  // tmax
-            0.0f,                    // rayTime
-            OptixVisibilityMask( 1 ),
-            OPTIX_RAY_FLAG_NONE,
-            RAIDANCE,      // SBT offset
-            RAY_TYPE_COUNT,          // SBT stride
-            RAIDANCE,      // missSBTIndex
-            u0,u1);
-
-    if( !occluded )
-        {
-            const float att = Ldist * Ldist;
-            const float A = length(cross(lightV1, lightV2));
-        weight = nDl * LnDl * A  / att;
-        }
-    }
-    prd.seed = seed;
-    prd.radiance += make_float3(5.0f, 5.0f, 5.0f)*weight* optixLaunchParams.global->lightScale * (glossy *make_float3(0.8f, 0.8f, 0.8f) / glossy_rays);
-}
-
-//GLASS
-
-extern "C" __global__ void __closesthit__radiance_glass()
+extern "C" __global__ void __closesthit__radiance__glass()
 {
     const TriangleMeshSBTData &sbtData
         = *(const TriangleMeshSBTData*)optixGetSbtDataPointer();
@@ -404,13 +272,13 @@ extern "C" __global__ void __closesthit__radiance_glass()
 
             //Achar o número de raios brilhantes, e o grau de brilho
             const float glossiness = optixLaunchParams.global->glossiness;
-            const int glossy_rays = optixLaunchParams.global->glossyRays;
+            const int glossyRays = optixLaunchParams.global->glossyRays;
             float3 glossy = make_float3(0.0f);
             float3 direcaoL;
             float3 reflectDir = reflect(optixGetWorldRayDirection(), normal);
             
             //Lançar os raios
-            for (int i = 0; i < glossy_rays; ++i) {
+            for (int i = 0; i < glossyRays; ++i) {
                 reflectPRD.radiance = make_float3(1.0f);
                 do{
                     const float z1 = rnd(seed);
@@ -470,40 +338,150 @@ extern "C" __global__ void __closesthit__radiance_glass()
 
 }
 
-extern "C" __global__ void __anyhit__radiance_glass()
+//Funções Usadas para o Metal
+
+extern "C" __global__ void __closesthit__radiance__metal()
 {
+
+    const TriangleMeshSBTData &sbtData
+      = *(const TriangleMeshSBTData*)optixGetSbtDataPointer();  
+
+    RadiancePRD &prd = *(RadiancePRD *)getPRD<RadiancePRD>();
+
+    // Recuperar Id Primitivo e Índices
+    const int   primID = optixGetPrimitiveIndex();
+    const uint3 index  = sbtData.index[primID];
+
+    // Obter Barycentric Coordinates
+    const float u = optixGetTriangleBarycentrics().x;
+    const float v = optixGetTriangleBarycentrics().y;
+
+    // Calcular o Vetor Normal
+    const float4 n
+        = (1.f-u-v) * sbtData.vertexD.normal[index.x]
+        +         u * sbtData.vertexD.normal[index.y]
+        +         v * sbtData.vertexD.normal[index.z];
+
+    // Normalizar o Vetor Normal
+    // O Vetor Normalizado é dado com a mesma direção
+    const float3 normal = normalize(make_float3(n));
+
+    // Posição da Interceção
+    const float3 &rayDir =  optixGetWorldRayDirection();
+    const float3 pos = optixGetWorldRayOrigin() + optixGetRayTmax() * rayDir;
+
+    if (prd.countEmitted && length(sbtData.emission) != 0)
+    {
+        prd.emitted = sbtData.emission;
+        return;
+    }
+    else
+    {
+        prd.emitted = make_float3(0.0f);
+    }
+
+    RadiancePRD afterPRD;
+    afterPRD.radiance = make_float3(2.0f);
+    afterPRD.seed = prd.seed;
+
+    uint32_t u0, u1;
+
+    // Pack Pointer essencial para o resultado
+    packPointer(&afterPRD, u0, u1);
+    
+    // Parte Essencial ao Path Tracing
+
+    // Definir/Determinar o Glossiness (Brilho) e o Glossy Rays (Raios Brilhantes)
+    const float glossiness = optixLaunchParams.global->glossiness;
+    const int glossyRays = optixLaunchParams.global->glossyRays;
+    
+    float3 glossy = make_float3(0.0f);
+    float3 direcaoL;
+    float3 reflectDir = reflect(optixGetWorldRayDirection(), normal);
+    
+    //Lançar os raios
+    for (int i = 0; i < glossyRays; ++i) {
+        afterPRD.radiance = make_float3(1.0f);
+        do{
+            const float z1 = rnd(prd.seed);
+            const float z2 = rnd(prd.seed);
+        
+            cosine_power_sample_hemisphere( z1, z2, direcaoL, glossiness );
+            Onb onb( reflectDir );
+            onb.inverse_transform( direcaoL );
+            prd.direction = direcaoL;
+            prd.origin    = pos;
+            prd.attenuation *= sbtData.diffuse ;
+           // prd.countEmitted = false;
+        } while (dot(direcaoL, normal) < 0.001);
+    
+        glossy += afterPRD.radiance;
+    
+    }
+
+    const float3 lightV1 = make_float3(0.47f, 0.0, 0.0f);
+    const float3 lightV2 = make_float3(0.0f, 0.0, 0.38f);
+
+    const float z1 = rnd(prd.seed);
+    const float z2 = rnd(prd.seed);
+    const float3 light_pos = make_float3(optixLaunchParams.global->lightPos) + lightV1 * z1 + lightV2 * z2;
+
+    // Calculate properties of light sample (for area based pdf)
+    const float  Ldist = length(light_pos - pos );
+    const float3 L     = normalize(light_pos - pos );
+    const float  nDl   = dot( normal, L );
+    const float3 Ln    = normalize(cross(lightV1, lightV2));
+    const float  LnDl  = -dot( Ln, L );
+
+    float weight = 0.0f;
+    if( nDl > 0.0f && LnDl > 0.0f )
+    {
+    uint32_t occluded = 0u;
+        optixTrace(optixLaunchParams.traversable,
+            pos,
+            L,
+            0.001f,         // tmin
+            Ldist - 0.01f,  // tmax
+            0.0f,                    // rayTime
+            OptixVisibilityMask( 1 ),
+            OPTIX_RAY_FLAG_NONE,
+            RAIDANCE,      // SBT offset
+            RAY_TYPE_COUNT,          // SBT stride
+            RAIDANCE,      // missSBTIndex
+            u0,u1);
+
+    if( !occluded )
+        {
+            const float att = Ldist * Ldist;
+            const float A = length(cross(lightV1, lightV2));
+        weight = nDl * LnDl * A  / att;
+        }
+    }
+
+    prd.radiance += make_float3(5.0f, 5.0f, 5.0f)*weight* optixLaunchParams.global->lightScale * (glossy *make_float3(0.8f, 0.8f, 0.8f) / glossyRays);
 }
 
-// miss sets the background color
-extern "C" __global__ void __miss__radiance_glass()
-{
-    RadiancePRD &prd = *(RadiancePRD*)getPRD<RadiancePRD>();
-    // set black as background color
-    prd.radiance = make_float3(0.0f, 0.0f, 0.0f);
-    prd.done = true;
-}
+// ############################################################
+// Shadow Rays
 
-// Shadow rays
-
-extern "C" __global__ void __closesthit__shadow_glass()
+extern "C" __global__ void __closesthit__shadow()
 {
     optixSetPayload_0( static_cast<uint32_t>(true));
 }
 
-// any hit for shadows
-extern "C" __global__ void __anyhit__shadow_glass()
+// Any Hit for Shadows
+extern "C" __global__ void __anyhit__shadow()
 {
 }
 
-// miss for shadows
-extern "C" __global__ void __miss__shadow_glass()
+// Miss for Shadows
+extern "C" __global__ void __miss__shadow()
 {
     optixSetPayload_0( static_cast<uint32_t>(false));
 }
 
-// -----------------------------------------------
+// ############################################################
 // Primary Rays
-
 
 extern "C" __global__ void __raygen__renderFrame()
 {
